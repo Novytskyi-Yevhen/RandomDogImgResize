578ee3fc43d6b815c9403ad11fbe91e031c31d26
Yevhen <serverdog1@gmail.com>
2021-07-15 19:21:52 +0300

    Feat: Create get route('/list/dog/images')
    This route returns a sorted list of images from the database depending on the specified option property.
    
    Fix issue #9

--- routes/list/dog/images.ts
+++ routes/list/dog/images.ts
@@ -1,12 +1,30 @@
-import console from "console";
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import DogImage, { IDogImage } from "../../../models/DogImage";
+
+const lodash = require("lodash");
 
 const router = Router();
 
-router.get("/list/dog/images", (req, res) => {
-  console.log(req.query);
+function renderImages(data: IDogImage[], option: string) {
+  return lodash
+    .sortBy(data, [option])
+    .map((element: IDogImage) => {
+      return `<div><img src = "data:image/png;base64,${element.buffer.toString(
+        "base64"
+      )}">
+    <p>Width: ${element.width} Height: ${
+        element.height
+      } File size: ${Buffer.byteLength(element.buffer)} bytes</p>
+    <a href="${element.mainImg}">Link to main photo</a></div>`;
+    })
+    .join("\n");
+}
 
-  res.json(req.query);
+router.get("/list/dog/images", async (req: Request, res: Response) => {
+  const dogImages = await DogImage.find({});
+  const option = String(req.query.option);
+  const render = renderImages(dogImages, option);
+  res.send(render);
 });
 
 module.exports = router;
